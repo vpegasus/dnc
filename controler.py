3f2065430b4e0417ef215253969a82483ed9d24c
vpegasus <pegasus.wenjia@foxmail.com>
2018-01-25 09:21:14 +0800

    'tst'

---
+++ controler.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python  
+# -*- coding: utf-8 -*-
+"""
+@author: Prince
+@file: controler.py 
+@time: 2018-1-25 09: 19
+@license: Apache License
+@contact: pegasus.wenjia@foxmail.com 
+"""
+class Controller:
+    def __init__(self):
